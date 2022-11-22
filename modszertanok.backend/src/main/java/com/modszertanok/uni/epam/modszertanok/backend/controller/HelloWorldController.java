e5135ee95dd432cc5789e4884513a61d8273d893
Kornel Filep <kornel.filep@gmail.com>
2022-11-22 09:07:44 +0100

    adding simple HelloWorld service as backend

---
+++ modszertanok.backend/src/main/java/com/modszertanok/uni/epam/modszertanok/backend/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.modszertanok.uni.epam.modszertanok.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    String greetings() {
+        return "Hello world!";
+    }
+
+    @GetMapping("/hello/{name}")
+    String greetByName(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+}
